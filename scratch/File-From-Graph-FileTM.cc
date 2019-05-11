be19fc9ecba5e2b33bdbd811d4ccfecfdbe54868
Vipul Harsh <vharsh2@illinois.edu>
2019-05-11 17:42:09 -0500

    Adding scripts for ns3

---
+++ scratch/File-From-Graph-FileTM.cc
@@ -0,0 +1,352 @@
+// Construction of Fat-tree Architecture
+// Authors: Linh Vu, Daji Wong
+
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2013 Nanyang Technological University 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: Linh Vu <linhvnl89@gmail.com>, Daji Wong <wong0204@e.ntu.edu.sg>
+ * Modifiedy by: Vipul Harsh <vipulharsh93@gmail.com>
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <cassert>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ns3/flow-monitor-module.h"
+#include "ns3/bridge-helper.h"
+#include "ns3/bridge-net-device.h"
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/csma-module.h"
+#include "ns3/ipv4-nix-vector-helper.h"
+#include "ns3/random-variable-stream.h"
+#include "ns3/ipv4-routing-table-entry.h"
+
+#include "topology.h"
+/*
+	- Adapted from the paper "Towards Reproducible Performance Studies of Datacenter Network Architectures Using An Open-Source Simulation Approach"
+
+	- The code is constructed in the following order:
+		1. Creation of Node Containers from a topology file
+		2. Initialize settings for On/Off Application/bulk.send application, traffic matrix read from a file
+		3. Connect hosts to top of the rack switches
+		4. Connect network switch using edges read from the topology file
+		6. Start Simulation
+
+	
+	- Simulation Settings, along with examples:
+		- Simulation running time: 100 seconds
+		- Packet size: 1024 bytes
+		- Data rate for packet sending: 1 Mbps
+		- Data rate for device channel: 1000 Mbps
+		- Delay time for device: 0.001 ms
+		- Communication pairs selection: Random Selection with uniform probability
+		- Traffic flow pattern: Exponential random traffic
+
+        - Statistics Output:
+                - Flowmonitor XML output file: Fat-tree.xml is located in the /statistics folder
+            
+
+*/
+
+using namespace ns3;
+using namespace std;
+NS_LOG_COMPONENT_DEFINE ("DC-Architecture");
+
+inline int truncateBytes(int bytes){
+   return (bytes < 10? 0 : bytes);
+}
+
+void echo_progress(double delay_seconds){
+    Simulator::Schedule (Seconds(delay_seconds), &echo_progress, delay_seconds);
+    cout<<"Finished: "<<Simulator::Now ().GetSeconds ()<<" seconds"<<endl;
+}
+
+
+int main(int argc, char *argv[])
+{
+   int nreqarg = 5;
+   if(argc <= nreqarg){
+      cout<<nreqarg-1<<" arguments required, "<<argc-1<<" given."<<endl;
+      cout<<"Usage> <exec> <topology_file> <tm_file> <result_file> <drop queue limit(e.g. 100 for a limit of 100 packets)> <data rate for on/off (e.g. 10 for 10 Mbps) >"<<endl;
+      exit(0);
+   }
+   string topology_filename = argv[1];
+   string tm_filename = argv[2];
+   string result_filename = argv[3];
+   int drop_queue_limit = atoi(argv[4]);
+   string on_off_datarate = argv[5];
+
+
+   cout<<"Running topology: "<<topology_filename<<", output result to "<<result_filename<<endl;
+   cout<<"Setting drop queue limit: "<<drop_queue_limit<<endl;
+
+  Config::SetDefault ("ns3::DropTailQueue::MaxPackets", UintegerValue (drop_queue_limit));
+
+//==== Simulation Parameters ====//
+   double simTimeInSec = 0.1;
+   double warmupTimeInSec = 1.0;
+   bool onOffApplication = false;	
+
+//=========== Define topology  ===========//
+//
+   Topology topology;
+   topology.readTopologyFromFile(topology_filename);
+   topology.readServerTmFromFile(tm_filename);
+
+// Initialize parameters for On/Off application
+//
+	int port = 9; //port for background app
+	int packetSizeBytes = 1500;
+	string dataRate_OnOff = on_off_datarate + "Mbps"; //"1Mbps";
+	//char maxBytes [] = "0" ; //"50000"; // "0"; // unlimited
+
+// Initialize parameters for PointToPoint protocol
+//
+	char dataRate [] = "10Gbps";
+	uint64_t delay_us = 5; //microseconds
+
+
+// Fail some links, by setting loss rates
+    int nfails = 2;
+    topology.chooseFailedLinks(nfails);
+    double silent_drop_rate = 0.001;
+	
+// Output some useful information
+//	
+    std::cout << "Total number of hosts =  "<< topology.total_host<<"\n";
+    if (onOffApplication){
+        std::cout << "On/Off flow data rate = "<< dataRate_OnOff<<"."<<endl;
+    }
+
+// Initialize Internet Stack and Routing Protocols
+//	
+	InternetStackHelper internet;
+	Ipv4NixVectorHelper nixRouting; 
+	Ipv4StaticRoutingHelper staticRouting;
+	Ipv4GlobalRoutingHelper globalRouting;
+	Ipv4ListRoutingHelper list;
+	//list.Add (staticRouting, 0);	
+	//list.Add (nixRouting, 10);	
+	list.Add (globalRouting, 20);	
+	internet.SetRoutingHelper(list);
+
+//=========== Creation of Node Containers ===========//
+
+    NodeContainer tors;				// NodeContainer for all ToR switches
+    tors.Create(topology.num_tor);
+    internet.Install(tors);
+
+    NodeContainer rackhosts[topology.num_tor];
+    for (int i=0; i<topology.num_tor;i++){  	
+        rackhosts[i].Create (topology.getNumHostsInRack(i));
+        internet.Install (rackhosts[i]);		
+    }
+
+//=========== Initialize settings for On/Off Application ===========//
+//
+
+// Generate traffics for the simulation
+//
+   cout<<"Creating application flows .... "<<endl;
+
+   double max_traffic = 0.0;
+   for(int i=0; i<topology.total_host; i++){
+      //cout<<"row.size(): "<<topology.serverTM[i].size()<<endl;
+      max_traffic = max(max_traffic, *std::max_element(topology.serverTM[i].begin(),topology.serverTM[i].end()));
+   }
+   double traffic_wt = 0.1;
+   cout<<"Max Traffic: "<<max_traffic<<", Weighted: "<<max_traffic * traffic_wt<<endl;
+
+   ApplicationContainer** flow_app = new ApplicationContainer*[topology.total_host];
+   for(int i=0; i<topology.total_host; i++)
+      flow_app[i] = new ApplicationContainer[topology.total_host];
+   int nflows = 0;
+   long long total_bytes=0;
+
+   double onTime = 10000.000, offTime = 0.000;
+   Ptr<ExponentialRandomVariable> rvOn = CreateObject<ExponentialRandomVariable> ();
+   rvOn->SetAttribute ("Mean", DoubleValue (onTime));
+   rvOn->SetAttribute ("Bound", DoubleValue (0.0));
+   Ptr<ExponentialRandomVariable> rvOff = CreateObject<ExponentialRandomVariable> ();
+   rvOff->SetAttribute ("Mean", DoubleValue (offTime));
+   rvOff->SetAttribute ("Bound", DoubleValue (0.0));
+   for(int i=0; i<topology.total_host; i++){
+      for(int j=0; j<topology.total_host; j++){
+         int bytes = truncateBytes((int)(topology.serverTM[i][j] * traffic_wt));
+         //cout<<"bytes: "<<bytes<<endl;
+         if(bytes < 1) continue;
+         nflows++;
+         total_bytes += bytes;
+         char* bytes_c = new char[50];
+         sprintf(bytes_c,"%d",bytes);
+         string bytes_s = bytes_c;
+         char *src_ip = topology.getHostIpAddress(i);
+         char *remote_ip = topology.getHostIpAddress(j);
+
+         OnOffHelper oo = OnOffHelper("ns3::TcpSocketFactory",Address(InetSocketAddress(Ipv4Address(remote_ip), port))); // ip address of server
+         BulkSendHelper source ("ns3::TcpSocketFactory", Address(InetSocketAddress(Ipv4Address(remote_ip), port)));
+         // Initialize On/Off Application with addresss of server
+         if(onOffApplication){
+            //cout<<"Initializing On Off flow"<<endl;
+            //Simulate flows that never stop by having a very large onTime
+            oo.SetAttribute("DataRate",StringValue (dataRate_OnOff));      
+            oo.SetAttribute("PacketSize",UintegerValue (packetSizeBytes));
+            //oo.SetAttribute ("Remote", Address(InetSocketAddress(Ipv4Address(remote_ip), port)));
+            oo.SetAttribute("OnTime", StringValue ("ns3::ConstantRandomVariable[Constant=1000.0]"));
+            oo.SetAttribute("OffTime", StringValue ("ns3::ConstantRandomVariable[Constant=0.0]"));
+            oo.SetAttribute("MaxBytes",StringValue (bytes_c));
+         }
+         //Initialize BulkSend application
+         else{
+            // Set the amount of data to send in bytes.  Zero is unlimited.
+            source.SetAttribute("SendSize",UintegerValue (packetSizeBytes));
+            source.SetAttribute("MaxBytes",StringValue (bytes_c));
+         }
+         //cout<<i<<"-->"<<j<<std::endl;
+         int irack = topology.getHostRack(i);
+         // Install On/Off Application on the source
+         if(onOffApplication){
+            NodeContainer onoff;
+            onoff.Add(rackhosts[irack].Get(topology.getHostIndexInRack(i)));
+            flow_app[i][j] = oo.Install (onoff);
+            //std::cout << "Finished creating On/Off traffic"<<"\n";
+         }
+         //For bulk send
+         else{
+            NodeContainer bulksend;
+            bulksend.Add(rackhosts[irack].Get(topology.getHostIndexInRack(i)));
+            flow_app[i][j] = source.Install (bulksend);
+            //flow_app[i][j].Start (Seconds (1.0));
+            //flow_app[i][j].Stop (Seconds (simTimeInSec));
+            //std::cout << "Finished creating Bulk send and Packet Sink Applications"<<"\n";
+         }
+      }
+   }
+   cout<<"Total Flows in the system: "<<nflows<<" carrying "<<total_bytes<<" bytes"<<endl;
+
+   for(int i=0;i<topology.total_host; i++){
+      //Create packet sink application on every server
+      PacketSinkHelper sink ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), port));
+      int irack = topology.getHostRack(i);
+      ApplicationContainer sinkApps = sink.Install (rackhosts[irack].Get(topology.getHostIndexInRack(i)));
+      sinkApps.Start (Seconds (0.0));
+      sinkApps.Stop (Seconds (warmupTimeInSec + simTimeInSec));
+   }
+   cout<<"Finished creating applications"<<endl;
+   
+
+// Initialize PointtoPointHelper for normal and failed links
+//	
+	PointToPointHelper p2p;
+  	p2p.SetDeviceAttribute ("DataRate", StringValue (dataRate));
+  	p2p.SetChannelAttribute ("Delay", TimeValue (MicroSeconds (delay_us)));
+    Ptr<RateErrorModel> rem = CreateObjectWithAttributes<RateErrorModel> (
+                                "ErrorRate", DoubleValue (silent_drop_rate),
+                                "ErrorUnit", EnumValue (RateErrorModel::ERROR_UNIT_PACKET));
+	/*
+    PointToPointHelper p2p_failed;
+  	p2p_failed.SetDeviceAttribute ("DataRate", StringValue (dataRate));
+  	p2p_failed.SetChannelAttribute ("Delay", TimeValue (MicroSeconds (delay_us)));
+  	p2p_failed.SetDeviceAttribute ("ReceiveErrorModel", PointerValue (rem));
+    */
+
+//=========== Connect switches to hosts ===========//
+//
+    topology.connect_switches_and_hosts(p2p, tors, rackhosts);
+	std::cout << "Finished connecting tors and hosts  "<< "\n";
+
+//=========== Connect  switches to switches ===========//
+//
+    topology.connect_switches_and_switches(p2p, rem, tors, silent_drop_rate);
+    std::cout << "Finished connecting switches and switches  "<< "\n";
+    std::cout << "------------- "<<"\n";
+
+//=========== Start the simulation ===========//
+//
+
+
+    Simulator::Schedule (Seconds(warmupTimeInSec), &echo_progress, simTimeInSec/100.0);
+
+    //Check if ECMP routing is working 
+    //Config::SetDefault("ns3::Ipv4GlobalRouting::FlowEcmpRouting", BooleanValue(true));
+    cout<<"Populating routing tables:"<<endl;
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+   Ptr<OutputStreamWrapper> routingStream = Create<OutputStreamWrapper> (&std::cout);
+   //for(int i=0; i<num_tor; i++)
+   //   globalRouting.PrintRoutingTableAt (Seconds (5.0), tors.Get(i), routingStream);
+
+   std::cout << "Start Simulation.. "<<"\n";
+   for (int i=0;i<topology.total_host;i++){
+      for (int j=0;j<topology.total_host;j++){
+         int bytes = truncateBytes((int)(topology.serverTM[i][j] * traffic_wt));
+         if(bytes < 1) continue;
+		   //flow_app[i][j].Start (Seconds (warmupTimeInSec));
+		   flow_app[i][j].Start (Seconds (warmupTimeInSec + rand() * simTimeInSec/RAND_MAX));
+  		   flow_app[i][j].Stop (Seconds (warmupTimeInSec + simTimeInSec));
+      }
+	}
+
+// Calculate Throughput using Flowmonitor
+//
+  	FlowMonitorHelper flowmon;
+	Ptr<FlowMonitor> monitor = flowmon.InstallAll();
+
+
+// Run simulation.
+//
+  	NS_LOG_INFO ("Run Simulation.");
+  	Simulator::Stop (Seconds(simTimeInSec+1.0));
+  	Simulator::Run ();
+
+  	monitor->CheckForLostPackets ();
+  	monitor->SerializeToXmlFile(result_filename, true, true);
+
+	std::cout << "Simulation finished "<<"\n";
+
+    for (int i=0;i<topology.total_host;i++){
+        for (int j=0;j<topology.total_host;j++){
+            int bytes = truncateBytes((int)(topology.serverTM[i][j] * traffic_wt));
+            if(bytes < 1) continue;
+            topology.print_flow_info(i, j, bytes, flow_app[i][j]);
+        }
+    }
+
+    //Print flow info before Destory() and Ip addresses after Destroy()
+  	Simulator::Destroy ();
+    topology.print_ip_addresses();
+
+  	NS_LOG_INFO ("Done.");
+
+	return 0;
+}
+
+
+
+
---
+++ topology/process_log.py
@@ -0,0 +1,185 @@
+import sys
+from heapq import heappush, heappop
+from multiprocessing import Process, Queue
+import pyximport; pyximport.install()
+import math
+import time
+import joblib
+from joblib import Parallel, delayed
+import random
+import numpy as np
+from scipy.optimize import minimize
+
+class Link:
+    def __init__(self, srcip, destip, src, dest):
+        self.srcip = srcip
+        self.destip = destip
+        self.src = src
+        self.dest = dest
+
+class Flow:
+    def __init__(self, src, srcip, dest, destip, nbytes, start_time, packets_sent, lost_packets, randomly_lost_packets):
+        self.src = src
+        self.srcip = srcip
+        self.dest = dest
+        self.destip = destip
+        self.nbytes = nbytes
+        self.start_time = start_time
+        self.packets_sent = packets_sent
+        self.lost_packets = lost_packets
+        self.randomly_lost_packets = randomly_lost_packets
+        self.paths = []
+        self.reverse_paths = []
+        self.path_taken = None
+        self.reverse_path_taken = None
+
+    def add_path(self, p):
+        self.paths.append(p)
+
+    def add_reverse_path(self, p):
+        self.reverse_paths.append(p)
+
+    def set_path_taken(self, p):
+        self.path_taken = p;
+
+    def set_reverse_path_taken(self, p):
+        self.reverse_path_taken = p
+    
+    def printinfo(self):
+        print("Flowid=", self.src, self.dest, self.nbytes, self.start_time, self.packets_sent, self.lost_packets, self.randomly_lost_packets)
+        for p in self.paths:
+            s = "flowpath"
+            if p == self.path_taken:
+                s += "_taken"
+            for n in p:
+                s += " " + str(n)
+            print(s)
+        for p in self.reverse_paths:
+            s = "flowpath_reverse"
+            if p == self.reverse_path_taken:
+                s += "_taken"
+            for n in p:
+                s += " " + str(n)
+            print(s)
+            
+
+    
+def convert_path(p, hostip_to_host, linkip_to_link, host_switch_ip):
+    path = []
+    assert (len(p) >= 1)
+    srchost = p[0]
+    destrack = p[-1]
+    assert (srchost in hostip_to_host and destrack in host_switch_ip)
+    for i in range(1, len(p)-1):
+        link = linkip_to_link[p[i]]
+        assert (link.srcip == p[i])
+        path.append(link.src)
+    path.append(host_switch_ip[destrack])
+    return path
+
+
+def process_logfile(filename):
+    flows = []
+    start_simulation = False
+    linkip_to_link = dict()
+    hostip_to_host = dict()
+    host_to_hostip = dict()
+    host_switch_ip = dict()
+    flow_route_taken = dict()
+    curr_flow = None
+    recording = False
+    with open(filename, encoding = "ISO-8859-1") as f:
+        flow = None
+        for line in f.readlines():
+            if "Failing_link" in line:
+                print(line.rstrip());
+                continue
+            tokens = line.split()
+            if (not start_simulation) and ("Start Simulation.." in line):
+                start_simulation = True
+            if not start_simulation:
+                continue;
+            elif "link_ip" in line:
+                srcip = tokens[1]
+                destip = tokens[2]
+                src = int(tokens[3])
+                dest = int(tokens[4])
+                linkip_to_link[srcip] = Link(srcip, destip, src, dest)
+                linkip_to_link[destip] = Link(destip, srcip, dest, src)
+            elif "host_ip" in line:
+                host_ip1 = tokens[1] #switch
+                host_ip2 = tokens[2] #host
+                host = int(tokens[3])
+                rack = int(tokens[4])
+                hostip_to_host[host_ip2] = host
+                host_to_hostip[host] = host_ip2
+                host_switch_ip[host_ip1] = rack
+            elif "hop_taken" in line:
+                srcip = tokens[1]
+                destip = tokens[2]
+                srcport = int(tokens[3])
+                destport = int(tokens[4])
+                if srcport > 1000000 or destport > 10000000 or "102.102.102.102" in srcip or "102.102.102.102" in destip:
+                    #junk entry due to arp maybe
+                    continue
+                hop_ip = tokens[6]
+                flow = (srcip, destip, srcport, destport)
+                if flow not in flow_route_taken:
+                    flow_route_taken[flow] = []
+                flow_route_taken[flow].append(hop_ip)
+            elif "Flowid" in line:
+                #Flowid 0 2 10.0.0.2 10.0.1.2 100000000 +1039438292.0ns 74215 0 0
+                src = int(tokens[1])
+                dest = int(tokens[2])
+                srcip = tokens[3]
+                destip = tokens[4]
+                srcport = int(tokens[5])
+                destport = int(tokens[6])
+                nbytes = int(tokens[7])
+                start_time = float(tokens[8].strip('+ns'))
+                packets_sent = int(tokens[9])
+                lost_packets = int(tokens[10])
+                randomly_lost_packets = int(tokens[11])
+                if (curr_flow != None and len(curr_flow.paths) > 0 and recording):
+                    flows.append(curr_flow)
+                curr_flow = Flow(src, srcip, dest, destip, nbytes, start_time, packets_sent, lost_packets, randomly_lost_packets)
+                flow_tuple = (srcip, destip, srcport, destport)
+                reverse_flow_tuple = (destip, srcip, destport, srcport)
+                if flow_tuple not in flow_route_taken or reverse_flow_tuple not in flow_route_taken:
+                    print("Ignoring: ", flow_tuple)
+                    recording = False
+                    continue
+                recording = True
+                curr_flow.set_path_taken(flow_route_taken[flow_tuple])
+                curr_flow.set_reverse_path_taken(flow_route_taken[reverse_flow_tuple])
+            elif "flowpath_reverse" in line:
+                #flowpath_reverse 0 2 1
+                path = []
+                for i in range(1, len(tokens)):
+                    path.append(int(tokens[i]))
+                if(len(path) > 0 and recording):
+                    curr_flow.add_reverse_path(path)
+            elif "flowpath" in line:
+                #flowpath 0 2 1
+                path = []
+                for i in range(1, len(tokens)):
+                    path.append(int(tokens[i]))
+                if(len(path) > 0 and recording):
+                    curr_flow.add_path(path)
+            else:
+                pass
+                #print("Unrecognized log statement: ", line)
+        if curr_flow != None and len(curr_flow.paths)>0: #log the previous flow
+            if curr_flow.packets_sent > 0:
+                flows.append(curr_flow)
+        for flow in flows:
+            flow.set_path_taken(convert_path(flow.path_taken, hostip_to_host, linkip_to_link, host_switch_ip))
+            flow.set_reverse_path_taken(convert_path(flow.reverse_path_taken, hostip_to_host, linkip_to_link, host_switch_ip))
+            flow.printinfo()
+
+#return {'failed_links':failed_links, 'flows':flows, 'links':links, 'inverse_links':inverse_links, 'link_statistics':link_statistics}
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    process_logfile(filename)
+

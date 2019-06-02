58e21d3ac6494f703ae8c8ee3650ee35c3923496
Vipul Harsh <vharsh2@illinois.edu>
2019-06-01 22:33:09 -0500

    adding directory for c++ implementation

---
+++ localization/c++/estimator.h
@@ -0,0 +1,23 @@
+#ifndef FAULT_LOCALIZE_ESTIMATOR
+#define FAULT_LOCALIZE_ESTIMATOR
+
+#include "flow.h"
+#include "link.h"
+
+struct EstimatorResult{
+    double precision;
+    double recall;
+    void *info;
+    EstimatorResult(double _precision, double _recall, void* _info==NULL):
+        precision(_precision), recall(_recall), info(_info){}
+};
+
+
+class Estimator{
+ public:
+    Estimator(){}
+
+    EstimatorResult localize(vector<Flow> &flows, vector<Link> &links);
+};
+
+#endif
